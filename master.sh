a6157d8ce48de2403de5ca33acd3787ea15e9504
wangbinbin <hellomsg_noreply@163.com>
2018-05-11 16:58:14 +0800

    start hdfs

--- master.sh
+++ master.sh
@@ -12,5 +12,8 @@ export SPARK_HOME=/spark
 
 find etc/hadoop/ -type f | xargs sed -i "s/\$HOST/$(hostname)/g"
 
+/hadoop/bin/hdfs namenode -format
+/hadoop/bin/start-dfs.sh
+
 cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
     --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT 
